7008482ab30bd300134593705d309a0897bd9d00
jinseisieko <jinseisieko@gmail.com>
2025-10-15 09:49:08 +0300

    Colored interface

---
+++ src/main/java/org/jinseisieko/evolution/basic/Colored.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package org.jinseisieko.evolution.basic;
+
+import java.awt.Color;
+
+/**
+ * Interface for objects that have color
+ * 
+ * @author jinseisieko
+ */
+public interface Colored {
+    Color getColor();
+    void setColor(Color color);
+}
