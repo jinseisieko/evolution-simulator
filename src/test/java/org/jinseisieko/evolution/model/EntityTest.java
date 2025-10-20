835811635fba57744937579fd0e56c5feda4a5f5
jinseisieko <jinseisieko@gmail.com>
2025-10-20 17:02:52 +0300

    nothing

--- src/test/java/org/jinseisieko/evolution/model/EntityTest.java
+++ src/test/java/org/jinseisieko/evolution/model/EntityTest.java
@@ -1,14 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// /*
+//  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+//  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+//  */
 
-package org.jinseisieko.evolution.model;
+// package org.jinseisieko.evolution.model;
 
-/**
- *
- * @author jinseisieko
- */
-public class Entity {
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
-}
+// /**
+//  *
+//  * @author jinseisieko
+//  */
+// public class EntityTest {
+//     @Test
+//     void cons
+// }
