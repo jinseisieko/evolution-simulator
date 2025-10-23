f4fb42c3ccc15f064f1c5bbf8d893b93f38ace27
jinseisieko <jinseisieko@gmail.com>
2025-10-23 14:52:09 +0300

    test LessThanQuestion for Brain

---
+++ src/test/java/org/jinseisieko/evolution/model/stubs/LessThanQuestion.java
@@ -0,0 +1,29 @@
+// src/test/java/org/jinseisieko/evolution/model/stubs/LessThanQuestion.java
+package org.jinseisieko.evolution.model.stubs;
+
+import org.jinseisieko.evolution.decisiontree.Question;
+
+public final class LessThanQuestion implements Question {
+    private final int number;
+
+    public LessThanQuestion(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LessThanQuestion q)) return false;
+        return Double.compare(q.number, number) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(number);
+    }
+        
+}
