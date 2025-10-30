f0b718d83844a0665416527c657192eb9db1d1be
jinseisieko <jinseisieko@gmail.com>
2025-10-30 17:10:39 +0300

    Food

---
+++ src/main/java/org/jinseisieko/evolution/base/Food.java
@@ -0,0 +1,32 @@
+// src/main/java/org/jinseisieko/evolution/base/Food.java
+package org.jinseisieko.evolution.base;
+
+import org.jinseisieko.evolution.basic.Point;
+
+public class Food extends Entity{ 
+    private double health;
+    private final double TIME_HEALTH_COST;
+
+    public Food(Point initialCoordinates, double size, double TIME_HEALTH_COST) {
+        super(initialCoordinates, size);
+        this.health = 1.0;
+        this.TIME_HEALTH_COST = TIME_HEALTH_COST;
+    }
+
+    @Override
+    public void updateEntity(double dt) {
+        this.health -= TIME_HEALTH_COST;
+    }
+
+    public double getTIME_HEALTH_COST() {
+        return TIME_HEALTH_COST;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+}
